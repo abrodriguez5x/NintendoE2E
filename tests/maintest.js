b75d6a11f2143c23b389b137e1a3b5f905b12569
abrodriguez5x <arodriguez.rgonzalez@gmail.com>
2021-06-25 17:59:22 -0500

    adding selectors

--- tests/maintest.js
+++ tests/maintest.js
@@ -1,14 +1,20 @@
+import { Selector } from 'testcafe'
+
+const searchButton = Selector('#nclood-nav > div.top-nav-container.pin > div.top-nav > div > div.search-flex > button > span');
+const searchInput = Selector('#search-input');
+
+
 fixture("Main Fixture")
-    .page("https://www.nintendo.com/")
+    .page("https://www.nintendo.com/");
 
 test("First Test", async t => {
     await t
-        .click('#nclood-nav > div.top-nav-container.pin > div.top-nav > div > div.search-flex > button > span')
-        .typeText('#search-input', "Zelda")
-        .pressKey('enter')
+        .click(searchButton)
+        .typeText(searchInput, "Zelda")
+        .pressKey('enter');
 
 
 
 
 
-})
+});
