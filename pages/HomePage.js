ec56cc1d699aa2c301cfcba1da85d7147ed0a886
abrodriguez5x <arodriguez.rgonzalez@gmail.com>
2021-06-28 16:58:09 -0500

    Homepage selectors added

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,6 +1,20 @@
 import { Selector, t } from "testcafe";
 
 class HomePage {
+    constructor() {
+
+        this.Finder = Selector('#nclood-nav > div.top-nav-container.pin > div.top-nav > div > div.search-flex > button > span');
+        this.GamesButton = Selector('#nclood-nav > div.top-nav-container.pin > nav > ul > li.nav-gamestore > button').withText('Games');
+        this.ShopOption = Selector('#nclood-nav > div.top-nav-container.pin.expanded > nav > ul > li.nav-gamestore.active.highlight > div > div.expanded-nav-gamestore.active > div > div > a:nth-child(1) > div').withText('Shop games');
+        this.HardwareButton = Selector('#nclood-nav > div.top-nav-container.pin > nav > ul > li.nav-gamestore > button').withText('Hardware');
+        this.HardwareOption = Selector('#nclood-nav > div.top-nav-container.pin.expanded > nav > ul > li.nav-switch.active > div > div.expanded-nav-switch.active > div > div > a.switch-system.large > div').withText('Nintendo Switch Lineup');
+        this.NewsButton = Selector('#nclood-nav > div.top-nav-container.pin > nav > ul > li.nav-newsevents > a > button').withText('News & Events');
+
+    }
+
+    get nintendoPlay() {
+        return Selector('#nclood-nav > div.top-nav-container.pin > nav > ul > li.nav-playnintendo > a > button');
+    }
 
 }
 
